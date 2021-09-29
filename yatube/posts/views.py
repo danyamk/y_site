692eede90d72fef5dd74dc940de152b0550f975f
Danya <danyaxdlol@gmail.com>
2021-09-29 21:07:09 +0300

    H

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -20,10 +20,13 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
 
