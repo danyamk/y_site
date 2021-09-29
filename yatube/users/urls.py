bc2790682d975da11bc029d0db2daef1b02b31ad
Danya <danyaxdlol@gmail.com>
2021-09-29 20:48:47 +0300

    H

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -7,11 +7,13 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'),
-          name='logout'),
+    path('logout/', 
+         LogoutView.as_view(template_name='users/logged_out.html'),
+         name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('login/', LoginView.as_view(template_name='users/login.html'),
-          name='login'),
-    path('password_reset/', PasswordResetView.as_view(),
-          name='password_reset_form'),
+    path('login/', 
+         LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('password_reset/', 
+         PasswordResetView.as_view(), name='password_reset_form'),
 ]
+
