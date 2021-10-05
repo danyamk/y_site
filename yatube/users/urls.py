133640a48589c71ff6bf5051ed56c7ea498f2439
Danya <danyaxdlol@gmail.com>
2021-10-05 11:01:56 +0300

    Hallo

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -8,7 +8,8 @@ app_name = 'users'
 
 urlpatterns = [
      path('signup/', views.SignUp.as_view(), name='signup'),
-     path('login/', LoginView.as_view(template_name='users/login.html'),
+     path('login/',
+          LoginView.as_view(template_name='users/login.html'),
           name='login'),
      path('logout/',
           LogoutView.as_view(template_name='users/logged_out.html'),
